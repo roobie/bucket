40142e4108645753a8ef8ccf089c678be94ef811
Björn Roberg <bjorn.roberg@gmail.com>
2015-11-25 11:40:40 +0100

    JS: add a function example for query string parameter retrieval

--- code/js/regex/query_params.js
+++ code/js/regex/query_params.js
@@ -5,3 +5,21 @@ var match = (/queryParameterName=([^&]*)&?/i).exec(location.href);
 var urlPart = "/foo/bar/baz.json?ID=9";
 var match = (/id=([^&]*)&?/i).exec(urlPart);
 // match =~= ["ID=9", "9"]
+
+
+var getQueryParameter = function (key, /** @optional */fromURL) {
+  // match the `key` next to a `=` that is followed by a group
+  // which can contain anythin but a `&`. Query string might
+  // terminate in a `&` that is not followed by anything
+  // so end with `&?`.
+  var re = key + '=([^&]*)&?';
+  var url = fromURL || location.search;
+  // remove the hash, if any.
+  url = url.split('#')[0];
+  var match = new RegExp(re, 'i').exec(url)
+  return {
+    key: key,
+    value: match[1],
+    pair: match[0]
+  }
+}
